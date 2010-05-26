0f147aeb62440c2341be631d7271aecf01b287ba
Zeljko Filipin <zeljko.filipin@gmail.com>
2010-05-26 12:53:26 +0200

    move right

--- pacman.rb
+++ pacman.rb
@@ -2,3 +2,12 @@ require "watir"
 
 b = Watir::Browser.start "http://www.google.com/pacman/"
 b.button(:value => "Insert Coin").click
+sleep 5
+b.send_keys "{TAB}"
+b.send_keys "{TAB}"
+b.send_keys "{TAB}"
+b.send_keys "{TAB}"
+b.send_keys "{TAB}"
+b.send_keys "{TAB}"
+b.send_keys "{TAB}"
+b.send_keys "{RIGHT}"
