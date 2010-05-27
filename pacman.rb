f84651371caec27a896ede0ce0377d22c6d51327
Zeljko Filipin <zeljko.filipin@gmail.com>
2010-05-27 16:56:56 +0200

    added comment how the script knows when to stop

--- pacman.rb
+++ pacman.rb
@@ -30,6 +30,8 @@ directions = {
   3 => "up",
   4 => "down"}
 
+# pacman is not visible when the game is over, but also when it dies, so do not
+# stop until there are not more pacmans left
 while pacman_visible(browser) or pacmans(browser) > 0 do
   # get random number from 1 to 4
   direction = directions[1 + rand(4)]
