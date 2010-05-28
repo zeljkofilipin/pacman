0fd3da7506d8b33dc1182c0ea85e37d24d1452c2
Zeljko Filipin <zeljko.filipin@gmail.com>
2010-05-28 23:45:06 +0200

    better pacman positions comments

--- pacman.rb
+++ pacman.rb
@@ -13,10 +13,10 @@ end
 def pacman_visible?(browser)
   pacman_html(browser) !~ /LEFT: -62px/
 end
-def pacman_vertical_position(browser) # 8-120
+def pacman_vertical_position(browser) # 8-top, 120-bottom
   pacman_html(browser).split("TOP: ")[1].split("px")[0].to_i
 end
-def pacman_horizontal_position(browser) # 8-448
+def pacman_horizontal_position(browser) # 8-left, 448-right
   pacman_html(browser).split("LEFT: ")[1].split("px")[0].to_i
 end
 def pacman_html(browser)
