ec94de972a905f18f6c0c7b81033e3d0eb772886
Zeljko Filipin <zeljko.filipin@gmail.com>
2010-05-31 14:01:52 +0200

    pacman extracts it's left and top positions from it's HTML

--- pacman.rb
+++ pacman.rb
@@ -1,7 +1,9 @@
 class Pacman
   def initialize
   end
-  def position(position_html)
+  def extract_positions_from_html(position_html)
+    [extract_position_from_html(position_html, "LEFT: "),
+      extract_position_from_html(position_html, "TOP: ")]
   end
   def extract_position_from_html(position_html, position)
     position_html.split('"')[1].split(position)[1].split("px")[0].to_i
--- pacman_spec.rb
+++ pacman_spec.rb
@@ -6,9 +6,8 @@ describe Pacman do
   end
 
   it "should extract it's position from it's HTML" do
-    pending
     position_html = "\r\n<DIV class=pcm-ac id=actor0 style=\"LEFT: 272px; OVERFLOW: hidden; TOP: 120px\"><IMG style=\"DISPLAY: block; LEFT: -2px; POSITION: relative; TOP: -2px\" src=\"http://www.google.com/logos/pacman10-hp-sprite.png\"></DIV>"
-    @pacman.position(position_html).should == [272, 120]
+    @pacman.extract_positions_from_html(position_html).should == [272, 120]
   end
   it "should extract left position from it's HTML" do
     position_html = "\r\n<DIV class=pcm-ac id=actor0 style=\"LEFT: 272px; OVERFLOW: hidden; TOP: 120px\"><IMG style=\"DISPLAY: block; LEFT: -2px; POSITION: relative; TOP: -2px\" src=\"http://www.google.com/logos/pacman10-hp-sprite.png\"></DIV>"
