0421291b331f7ae8635202069ee6f37bffef49ec
Zeljko Filipin <zeljko.filipin@gmail.com>
2010-05-31 13:54:39 +0200

    refactoring: introduced extract_position_from_html

--- pacman.rb
+++ pacman.rb
@@ -4,9 +4,12 @@ class Pacman
   def position(position_html)
   end
   def left_position(position_html)
-    position_html.split('"')[1].split("LEFT: ")[1].split("px")[0].to_i
+    extract_position_from_html(position_html, "LEFT: ")
   end
   def top_position(position_html)
-    position_html.split('"')[1].split("TOP: ")[1].split("px")[0].to_i
+    extract_position_from_html(position_html, "TOP: ")
+  end
+  def extract_position_from_html(position_html, position)
+    position_html.split('"')[1].split(position)[1].split("px")[0].to_i
   end
 end
