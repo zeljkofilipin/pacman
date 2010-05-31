1991132c4ab44e513f64111553e4d2b8da8446d9
Zeljko Filipin <zeljko.filipin@gmail.com>
2010-05-31 13:51:22 +0200

    pacman extracts top position from it's HTML

--- pacman.rb
+++ pacman.rb
@@ -6,4 +6,7 @@ class Pacman
   def left_position(position_html)
     position_html.split('"')[1].split("LEFT: ")[1].split("px")[0].to_i
   end
+  def top_position(position_html)
+    position_html.split('"')[1].split("TOP: ")[1].split("px")[0].to_i
+  end
 end
--- pacman_spec.rb
+++ pacman_spec.rb
@@ -14,5 +14,9 @@ describe Pacman do
     position_html = "\r\n<DIV class=pcm-ac id=actor0 style=\"LEFT: 272px; OVERFLOW: hidden; TOP: 120px\"><IMG style=\"DISPLAY: block; LEFT: -2px; POSITION: relative; TOP: -2px\" src=\"http://www.google.com/logos/pacman10-hp-sprite.png\"></DIV>"
     @pacman.left_position(position_html).should == 272
   end
+  it "should extract top position from it's HTML" do
+    position_html = "\r\n<DIV class=pcm-ac id=actor0 style=\"LEFT: 272px; OVERFLOW: hidden; TOP: 120px\"><IMG style=\"DISPLAY: block; LEFT: -2px; POSITION: relative; TOP: -2px\" src=\"http://www.google.com/logos/pacman10-hp-sprite.png\"></DIV>"
+    @pacman.top_position(position_html).should == 120
+  end
 end
 
