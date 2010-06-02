6a82bf235cb807d148ff02fb78cb50d5188de6a1
Zeljko Filipin <zeljko.filipin@gmail.com>
2010-06-02 16:50:09 +0200

    added is the position empty to coordinates and types

--- coordinates_and_type.rb
+++ coordinates_and_type.rb
@@ -7,15 +7,22 @@ end
 def klass(html)
   html.split("=")[1].split(" ")[0]
 end
+def empty?(html)
+  if html =~ /DISPLAY: none/
+    true
+  else
+    false
+  end
+end
 
 require "benchmark"
 Benchmark.bm do |x|
   x.report do
-    browser.div(:id => "pcm-p").html.each do |div|
+    browser.div(:id => "pcm-p").html.each do |html|
       begin
-      puts "[#{coordinate(div, 'LEFT: ')}, #{coordinate(div, 'TOP: ')}] => '#{klass(div)}',"
+      puts "[#{coordinate(html, 'LEFT: ')}, #{coordinate(html, 'TOP: ')}] => ['#{klass(html)}', #{empty?(html)}],"
       rescue
-        puts "can not process: #{div.inspect}"
+        puts "can not process: #{html.inspect}"
       end
     end
   end
