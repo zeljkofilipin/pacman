a05a3658c84f0eb4b5ef1303015355623a033e8e
Zeljko Filipin <zeljko.filipin@gmail.com>
2012-07-24 09:28:08 +0200

    cleanup: there is no need to send tab keys to the browser

--- lib/1/pacman.rb
+++ lib/1/pacman.rb
@@ -1,6 +1,3 @@
-def tab(browser)
-  browser.send_keys "{TAB}"
-end
 def go(browser, direction)
   output(direction)
   browser.send_keys "{#{direction.upcase}}"
@@ -139,9 +136,6 @@ while true do
   # start the game
   browser.button(:value => "Insert Coin").click
   sleep 1
-
-  # move focus to the game
-  7.times {tab(browser)}
 
   directions = {
     1 => "right",
